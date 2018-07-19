d4d6c527b402d1586c1737d70c2361e8426008c5
Eduard Sachava <liberumed@gmail.com>
2018-07-19 12:09:55 +0700

    Change phoenix version in deps

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Shield.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
+    [{:phoenix, "~> 1.3.3"},
      {:phoenix_ecto, "~> 3.2.1"},
      {:gettext, "~> 0.13"},
      {:cowboy, "~> 1.0"},
